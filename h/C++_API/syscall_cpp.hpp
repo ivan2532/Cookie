5a9ee66e68d160d5eeb9ab2af439cd200f333a18
Ivan Božović <ivan.bozovic02@gmail.com>
2023-05-21 16:45:10 -0700

    Mark Semaphore ctor explicit

--- h/C++_API/syscall_cpp.hpp
+++ h/C++_API/syscall_cpp.hpp
@@ -35,7 +35,7 @@ private:
 class Semaphore
 {
 public:
-    Semaphore (unsigned init = 1);
+    explicit Semaphore (unsigned init = 1);
     virtual ~Semaphore ();
     int wait ();
     int signal ();
